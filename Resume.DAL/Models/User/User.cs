bea74848ca015a80b368e66c5c239c3a7dd7ca32
Invisible <Invisible@DESKTOP-5VBA7GF>
2026-01-06 13:29:35 +0330

    Users Model implemented , context added to project

---
+++ Resume.DAL/Models/User/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resume.DAL.Models.User
+{
+    public class User
+    {
+        #region Properties 
+        public int Id { get; set; }
+        public string Email { get; set; } 
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Mobile { get; set; }
+        public string Password { get; set; }
+        public DateTime RegisterDate { get; set; }
+        public bool IsActive { get; set; }
+
+        #endregion
+    }
+}
