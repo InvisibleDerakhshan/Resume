425c5e7ecc759a0c4654e5da073bca3d3c521422
Invisible <Invisible@DESKTOP-5VBA7GF>
2026-02-20 13:27:13 +0330

    list view implemented and paging used in it

--- Resume.DAL/ViewModels/Common/Paging.cs
+++ Resume.DAL/ViewModels/Common/Paging.cs
@@ -90,13 +90,14 @@ namespace Resume.DAL.ViewModels.Common
             return this;
         }
 
-        public class PagingViewModel
-        {
-            public int Page { get; set; }
-            public int PageCount { get; set; }
-            public int StartPage { get; set; }
-            public int EndPage { get; set; }
+       
+    }
+    public class PagingViewModel
+    {
+        public int Page { get; set; }
+        public int PageCount { get; set; }
+        public int StartPage { get; set; }
+        public int EndPage { get; set; }
 
-        }
     }
 }
